510bb584b77906c3c0589e09dd7179fa395aba90
alexjun12 <alexjun12@naver.com>
2022-03-14 02:58:03 +0900

    22_03_14

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div>
+  <div v-show="confirmText === '엄준식'">
+    <h1>엄</h1>
+    <img :src="imgSrc" />
   </div>
+  <div v-show="confirmText !== '엄준식'">
+    <h1>암호입력</h1>
+    <input type="text" v-model="gotText" @keyup.enter="submit" />
+  </div>
+</div>
 </template>
+<script>
+export default {
+  name: 'NewVue',
+  components: {},
+  data() {
+    return{
+      imgSrc: "https://i.ytimg.com/vi/f08PnZA9AyY/hqdefault.jpg",
+      gotText: '',
+    };
+  },
+  computed: {
+    confirmText () {
+      return this.gotText;
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    submit() {
+      this.gotText = '';
+    }
+  }
+}
+</script>
