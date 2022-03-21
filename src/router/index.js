c9c7e025cb7d962a0abecd70611fefa4df77dfbc
alexjun12 <alexjun12@naver.com>
2022-03-22 00:05:41 +0900

    22_03_22

--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,82 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 //import Contact from '../views/ContactView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about", webpackPrefetch:true  */ '../views/AboutView.vue')
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(
+        /* webpackChunkName: "about", webpackPrefetch:true  */ "../views/AboutView.vue"
+      ),
   },
   {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import(/* webpackChunkName: "contact", webpackPrefetch:true */ '../views/ContactView.vue')
+    path: "/contact",
+    name: "Contact",
+    component: () =>
+      import(
+        /* webpackChunkName: "contact", webpackPrefetch:true */ "../views/ContactView.vue"
+      ),
   },
   {
-    path: '/basic',
-    name: 'Basic',
-    component: () => import(/* webpackChunkName: "basic", webpackPrefetch:true */ '../views/BasicView.vue')
+    path: "/basic",
+    name: "Basic",
+    component: () =>
+      import(
+        /* webpackChunkName: "basic", webpackPrefetch:true */ "../views/BasicView.vue"
+      ),
   },
   {
-    path: '/databinding',
-    name: 'DataBinding',
-    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/DataBinding.vue')
+    path: "/databinding",
+    name: "DataBinding",
+    component: () =>
+      import(
+        /* webpackChunkName: "databinding", webpackPrefetch:true */ "../views/DataBinding.vue"
+      ),
   },
   {
-    path: '/databindinghtml',
-    name: 'DataBindingHtml',
-    component: () => import(/* webpackChunkName: "databindinghtml", webpackPrefetch:true */ '../views/DataBindingHtml.vue')
+    path: "/databindinghtml",
+    name: "DataBindingHtml",
+    component: () =>
+      import(
+        /* webpackChunkName: "databindinghtml", webpackPrefetch:true */ "../views/DataBindingHtml.vue"
+      ),
   },
   {
-    path: '/example',
-    name: 'Example',
-    component: () => import(/* webpackChunkName: "example", webpackPrefetch:true */ '../views/Example.vue')
+    path: "/example",
+    name: "Example",
+    component: () =>
+      import(
+        /* webpackChunkName: "example", webpackPrefetch:true */ "../views/Example.vue"
+      ),
   },
   {
-    path: '/serverdata',
-    name: 'ServerData',
-    component: () => import(/* webpackChunkName: "serverdata", webpackPrefetch:true */ '../views/ServerData.vue')
+    path: "/serverdata",
+    name: "ServerData",
+    component: () =>
+      import(
+        /* webpackChunkName: "serverdata", webpackPrefetch:true */ "../views/ServerData.vue"
+      ),
   },
-]
+  {
+    path: "/mixin",
+    name: "MixinTest",
+    component: () =>
+      import(
+        /* webpackChunkName: "mixin", webpackPrefetch:true */ "../views/MixinTest.vue"
+      ),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/MixinTest.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <button type="button" @click="getProductList">조회</button>
+        <table>
+            <thead>
+                <tr>
+                    <th>제품먕</th>
+                    <th>가격</th>
+                    <th>배송료</th>
+                    <th>카테고리</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="i" v-for="(product, i) in productList">
+                    <td>{{product.product_name}}</td>
+                    <td>{{product.price}}</td>
+                    <td>{{product.delivery_price}}</td>
+                    <td>{{product.category}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+import ApiMixin from '../api.js';
+
+export default {
+    name: 'ServerData',
+    mixins: [ApiMixin],
+    components: {},
+    data() {
+        return{
+        productList: [],
+        };
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {
+        async getProductList() {
+            this.productList = await this.$callApi('https://1fd3a76e-bf93-4139-8c5d-b7000888b7da.mock.pstmn.io/productList','get',{});
+            console.log(this.productList);
+        }
+    }
+}
+</script>
